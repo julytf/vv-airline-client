d532701dd55d00b04ddac2fb4bf962e82fa3c356
julyyvvvvv <julytf.liyc@gmail.com>
2024-01-21 10:33:32 +0700

    style: add postcss, tailwind, fontawesome

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
+import tailwindcssForm from '@tailwindcss/forms'
+import colors from 'tailwindcss/colors'
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        primary: colors.sky['400'],
+        ['primary-lighter']: colors.sky['300'],
+        ['primary-darker']: colors.sky['500'],
+        secondary: colors.sky['500'],
+      },
       fontFamily: {
-        popi: 'Poppins',
-        meri: 'Merriweather',
+        // popi: 'Poppins',
+        // meri: 'Merriweather',
+        robo: 'Roboto',
+        nuni: 'Nunito',
       },
-      fontSize: {
-      },
-      width: {
-      },
+      fontSize: {},
+      width: {},
     },
   },
-  plugins: [],
+  plugins: [
+    // tailwindcssForm
+  ],
   darkMode: 'class',
+  // important: true,
 }
